6d2ca71c5d9fdf498a462f7e7a61592047d7d704
IvanSynyshyn <sg8l@meta.ua>
2016-07-22 15:37:54 +0300

    мінімальна робоча програма

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,17 +1,34 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DataBase {
+	private static final String url = "jdbc:postgresql://localhost:5432/Ivan";
+	private static final String user = "postgres";
+	private static final String password = "livfan";
+	private static Connection con;
+	private static Statement st;
+	private static ResultSet rs;
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		try {
-			Class.forName("org.postgresql.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Ivan", "paisley", "livfan");
-conn.
-		} catch (ClassNotFoundException e) {
+			con = DriverManager.getConnection(url, user, password);
+			st = con.createStatement();
+			rs = st.executeQuery("select * from public.\"TBF\" order by salary desc");
+			while (rs.next()) {
+				System.out.println(rs.getString(2) + " " + rs.getInt(4));
+			}
+		} catch (SQLException e) {
 			System.out.println("Where is your MySQL JDBC Driver?");
 			e.printStackTrace();
-			return;
+		} finally {
+
+			try {
+				con.close();
+			} catch (SQLException se) {
+				se.printStackTrace(); }
 		}
 	}
-
 }
