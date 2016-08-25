0c30ef3e3f9af828becf894f59122a0b686a0baa
IvanSynyshyn <sg8l@meta.ua>
2016-08-25 13:15:41 +0300

    Change main
    delete thread

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,39 +1,22 @@
-
+import java.io.IOException;
 
 public class DataBase {
 	
 
 	public static void main(String[] args) {
-		Runnable job = new Runnable() {
-			
-			@Override
-			public void run() {
-				ReaderSQL rd = new ReaderSQL();
-				rd.readSQL();
-				System.out.println("Done by " + Thread.currentThread().getName());
-			}
-		};
-		Thread thread1 = new Thread(job, "thread1");
-		Thread thread2 = new Thread(job, "thread2");
-		Thread thread3 = new Thread(job, "thread3");
 		
-		thread1.start();
-		thread2.start();
-		thread3.start();
+		ReaderSQL rd = new ReaderSQL();
+		rd.readSQL();
 		
+		CreateTable ct = new CreateTable();
+		ct.createTable();
 		
-//		ReaderSQL rd = new ReaderSQL();
-//		rd.readSQL();
-		
-//		CreateTable ct = new CreateTable();
-//		ct.createTable();
-		
-//		Changes ch = new Changes();
-//		try {
-//			ch.addLine();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		Changes ch = new Changes();
+		try {
+			ch.addLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
